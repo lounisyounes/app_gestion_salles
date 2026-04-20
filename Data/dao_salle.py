7834a1d9164449b94cdebea16bf4d5e6023b7cbc
Lounis Younes <youneslounis887@gmail.com>
2026-04-19 21:24:58 -0400

    Ajout de la methode insert_salle

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -13,3 +13,14 @@ class DataSalle:
             database=config["database"]
         )
         return conn
+
+    def insert_salle(self, salle):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        query = "INSERT INTO salle (code, libelle, type, capacite) VALUES (%s, %s, %s, %s)"
+        values = (salle.code, salle.libelle, salle.type, salle.capacite)
+        cursor.execute(query, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
