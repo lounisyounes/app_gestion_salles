eb27a0933a92a593667b552d83df86161df5cdf0
Lounis Younes <youneslounis887@gmail.com>
2026-04-19 21:50:09 -0400

    Ajout des methodes CRUD de l interface graphique

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+from tkinter import messagebox
+from models.salle import Salle
 from services.services_salle import ServiceSalle
 
 class ViewSalle(ctk.CTk):
@@ -34,14 +36,58 @@ class ViewSalle(ctk.CTk):
         self.cadreAction = ctk.CTkFrame(self, corner_radius=10)
         self.cadreAction.pack(pady=10, padx=10, fill="x")
 
-        self.btn_ajouter = ctk.CTkButton(self.cadreAction, text="Ajouter")
+        self.btn_ajouter = ctk.CTkButton(self.cadreAction, text="Ajouter", command=self.ajouter_salle)
         self.btn_ajouter.grid(row=0, column=0, padx=10, pady=10)
 
-        self.btn_modifier = ctk.CTkButton(self.cadreAction, text="Modifier")
+        self.btn_modifier = ctk.CTkButton(self.cadreAction, text="Modifier", command=self.modifier_salle)
         self.btn_modifier.grid(row=0, column=1, padx=10, pady=10)
 
-        self.btn_supprimer = ctk.CTkButton(self.cadreAction, text="Supprimer")
+        self.btn_supprimer = ctk.CTkButton(self.cadreAction, text="Supprimer", command=self.supprimer_salle)
         self.btn_supprimer.grid(row=0, column=2, padx=10, pady=10)
 
-        self.btn_rechercher = ctk.CTkButton(self.cadreAction, text="Rechercher")
+        self.btn_rechercher = ctk.CTkButton(self.cadreAction, text="Rechercher", command=self.rechercher_salle)
         self.btn_rechercher.grid(row=0, column=3, padx=10, pady=10)
+
+    def ajouter_salle(self):
+        salle = Salle(
+            self.entry_code.get(),
+            self.entry_libelle.get(),
+            self.entry_type.get(),
+            int(self.entry_capacite.get())
+        )
+        succes, message = self.service_salle.ajouter_salle(salle)
+        if succes:
+            messagebox.showinfo("Ajout", message)
+        else:
+            messagebox.showerror("Ajout", message)
+
+    def modifier_salle(self):
+        salle = Salle(
+            self.entry_code.get(),
+            self.entry_libelle.get(),
+            self.entry_type.get(),
+            int(self.entry_capacite.get())
+        )
+        succes, message = self.service_salle.modifier_salle(salle)
+        if succes:
+            messagebox.showinfo("Modification", message)
+        else:
+            messagebox.showerror("Modification", message)
+
+    def supprimer_salle(self):
+        code = self.entry_code.get()
+        self.service_salle.supprimer_salle(code)
+        messagebox.showinfo("Suppression", "Salle supprimee avec succes")
+
+    def rechercher_salle(self):
+        code = self.entry_code.get()
+        salle = self.service_salle.rechercher_salle(code)
+        if salle:
+            self.entry_libelle.delete(0, "end")
+            self.entry_libelle.insert(0, salle.libelle)
+            self.entry_type.delete(0, "end")
+            self.entry_type.insert(0, salle.type)
+            self.entry_capacite.delete(0, "end")
+            self.entry_capacite.insert(0, salle.capacite)
+        else:
+            messagebox.showerror("Recherche", "Salle introuvable")
