7022cc48d993cbdd5b99f1d617f46b23cfe0e7ff
Lounis Younes <youneslounis887@gmail.com>
2026-04-19 21:23:51 -0400

    Ajout de la methode get_connection

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -1,2 +1,15 @@
+import json
+import mysql.connector
+
 class DataSalle:
-    pass
+    def get_connection(self):
+        with open("Data/config.json", "r") as file:
+            config = json.load(file)
+
+        conn = mysql.connector.connect(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+        return conn
