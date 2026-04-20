1ec7abc47184d90103467de543be3743e1485363
Lounis Younes <youneslounis887@gmail.com>
2026-04-19 21:43:05 -0400

    Creation de la structure initiale de l interface graphique

---
+++ views/view_salle.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+from services.services_salle import ServiceSalle
+
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Gestion des salles")
+        self.geometry("600x400")
+
+        self.service_salle = ServiceSalle()
