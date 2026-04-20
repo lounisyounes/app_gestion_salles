986942f7a47d806a6a5a9ad02d6076a13786a01b
Lounis Younes <youneslounis887@gmail.com>
2026-04-19 21:29:17 -0400

    Ajout de la methode get_salles

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -56,3 +56,18 @@ class DataSalle:
         if row:
             return Salle(row[0], row[1], row[2], row[3])
         return None
+
+    def get_salles(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        query = "SELECT * FROM salle"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        salles = []
+        for row in rows:
+            salles.append(Salle(row[0], row[1], row[2], row[3]))
+        return salles
+
